b94c4371569e75929a3b76310ff4302d411b8489
Felix Kaaman <trundmatu@gmail.com>
2015-12-23 11:49:52 +0100

    Add day 1 part 2

--- 1/main.c
+++ 1/main.c
@@ -2,10 +2,11 @@
 
 int main()
 {
-    int floor = 0;
+    int floor = 0, down = 0, i = 0;
     char c;
 
-    while ((c = fgetc(stdin)) != EOF && (floor += (c == '(' ? 1 : -1), 1));
+    while ((c = fgetc(stdin)) != EOF && (++i, floor += (c == '(' ? 1 : -1), 1))
+        if (floor < 0 && !(down++)) printf("Santa went down at %d\n", i);
 
     printf("Santa is on floor %d\n", floor);
     return 0;
